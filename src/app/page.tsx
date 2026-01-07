d2fe5ed812b9e35fbcd1b2404d9710d4a42d03b2
mpalmer79 <mpalm2025@gmail.com>
2026-01-07 13:58:51 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -724,7 +724,7 @@ export default function HomePage() {
                     <svg className="w-5 h-5 text-green-500 mr-2 mt-0.5 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
                       <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                     </svg>
-                    <span className="text-gray-600">{plan.locations} location{plan.locations > 1 ? 's' : ''}</span>
+                    <span className="text-gray-600">{plan.locations === 1 ? '1 location' : `Up to ${plan.locations} locations`}</span>
                   </li>
                   <li className="flex items-start">
                     <svg className="w-5 h-5 text-green-500 mr-2 mt-0.5 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
