b7d84aa798726ae846289dbc0c3f6cbb0f114281
mpalmer79 <mpalm2025@gmail.com>
2026-01-07 08:51:30 -0500

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['lh3.googleusercontent.com', 's3-media0.fl.yelpcdn.com'],
+  },
+}
+
+module.exports = nextConfig
