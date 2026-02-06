cd9f44f96bf658409f96206a7ad656fc58cf467b
mpalmer79 <mpalm2025@gmail.com>
2026-02-06 13:30:21 -0500

    Update index.ts

--- src/components/landing/index.ts
+++ src/components/landing/index.ts
@@ -1,1 +1,10 @@
+export { default as Navbar } from './Navbar';
+export { default as Hero } from './Hero';
+export { default as ReviewShowcase } from './ReviewShowcase';
+export { default as IndustryBanner } from './IndustryBanner';
+export { default as HowItWorks } from './HowItWorks';
+export { default as Features } from './Features';
+export { default as Pricing } from './Pricing';
+export { default as CTASection } from './CTASection';
+export { default as Footer } from './Footer';
 
